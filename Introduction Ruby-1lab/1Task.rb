b4e19ab10d4f5c95f55f1764f5145e19ddb2fd73
StrongerProgrammer7 <swat55551@gmail.com>
2023-02-04 23:21:30 +0300

    #TDP-1 Introduction finish\n complete task2,3

--- Introduction Ruby-1lab/1Task.rb
+++ Introduction Ruby-1lab/1Task.rb
@@ -1,1 +1,21 @@
+#1Task
 puts "Hello world!"
+#2Task
+nameUser = ARGV[0]
+puts "Hello, #{ARGV[0]}"
+puts "Your favority language?"
+favoriteLanguage = STDIN.gets.chomp
+if (favoriteLanguage == "Ruby" || favoriteLanguage == "ruby") then
+	puts "You bad programmer"
+else
+	puts "Good,good!"
+end
+#Task3
+puts "Input command Ruby:"
+commandRuby = STDIN.gets.chomp
+puts "Input command OS(Linux)"
+commandLinux = STDIN.gets.chomp
+puts "Execution ruby.."
+system "ruby -e \'#{commandRuby}\'"
+puts "Execution OS.."
+system commandLinux
