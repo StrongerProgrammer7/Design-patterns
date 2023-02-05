4d456d726a1bca2195bd3303a3fcf96bac9e6d25
StrongerProgrammer7 <swat55551@gmail.com>
2023-02-05 11:32:05 +0300

    '#TDP-1/2/4 Task 3 1.Add solution method 3 2.Add method: sumDigits,setDigitsNum isOddDigits 3.Change method Task1 and Task2

--- Introduction Ruby-1lab/2Task.rb
+++ Introduction Ruby-1lab/2Task.rb
@@ -1,5 +1,5 @@
 #Common method----------------------------
-def primeDevider(num)
+def isPrimeDevider(num)
   for i in 2..Math.sqrt(num)
 	if(num%i==0) then
 	  return 0
@@ -8,47 +8,61 @@ def primeDevider(num)
 return 1
 end
 
-def primeDivisorNumber(num)
+def setDivisorNumber(num)
 	divisors = Array.new
 	for i in 1..((num**0.5)+1)
-		if(i*i==num and primeDevider(i)==1) then
+		if(i*i==num) then
 			divisors.push(i)
 		else
-			if(num%i==0 and primeDevider(i)==1) then
+			if(num%i==0) then
 				divisors.push(i)
 				temp=num/i
 				temp=temp.round
-				if(primeDevider(temp)==1)then
-					divisors.push(temp)
-				end
+				divisors.push(temp)
 			end
 		end
 	end
 	return divisors
 end
 
-#Task1---------------------------------------------
-def sumPrimeDivisor(num)
-  sum = 0
-  divisors = primeDivisorNumber(num)
-  divisors.each { |div| sum += div}
-  return sum
+def isOddDigit(digit)
+	return digit%2==0 ? 0 : 1
 end
 
-#puts sumPrimeDivisor(25)
+def setDigitsNumber(num)
+	setDigits = Array.new
+	while(num!=0)
+		setDigits.push(num%10)
+		num /=10
+	end
+	return setDigits
+end
+
+def sumDigitsNumber(num)
+	return setDigitsNumber(num).sum
+end
+
+#Task1---------------------------------------------
+def sumPrimeDivisorNumber(num)
+  return setDivisorNumber(num).sum { |div| if isPrimeDevider(div)==1 then div else 0 end }
+  #return setDivisorNumber(num).each { |div| sum += if isPrimeDevider(div)==1 then div else 0 end }
+end
+
+#puts sumPrimeDivisorNumber(25)
 
 #Task2---------------------------------------------
 def countOddDigitsNumber(num,minSizeDigit=nil)
-	count=0
-	while(num!=0)
-		temp=num%10
-		if(temp>minSizeDigit) then
-			count = count+1
-		end
-		num /=10
-	end
-return count
+	return setDigitsNumber(num).count { |dig| isOddDigit(dig)==1 and dig>minSizeDigit }
 end
 
 #puts countOddDigitsNumber(1924924,3)
+
 #Task3---------------------------------------------
+def multDivNumberSumDigLessSumDigOriginalNum(num)
+	sumDigOriginalNumber = sumDigitsNumber(num)
+	mult = 1 
+	setDivisorNumber(num).each { |div| if sumDigitsNumber(div) < sumDigOriginalNumber then mult*=div end}
+	return mult
+end
+
+puts multDivNumberSumDigLessSumDigOriginalNum(36)
