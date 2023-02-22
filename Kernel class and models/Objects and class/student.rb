e0b975b4bb5815404274323afd527e287d0f9aca
StrongerProgrammer7 <swat55551@gmail.com>
2023-02-22 20:26:09 +0300

    '#TDP - 2/8/6 Validation Valid phone,name,surname,lastname,mail,git,telegram Check methods is method's class

--- Kernel class and models/Objects and class/student.rb
+++ Kernel class and models/Objects and class/student.rb
@@ -3,8 +3,12 @@ class Student
 	attr_reader :ID
 
 	def initialize(surname,name,lastname, phone:nil, telegram:nil,mail:nil,git:nil)
-		self.surname = surname
-		self.name = name
+		if( Student.check_letter(surname) ==nil && Student.check_letter(name)==nil && Student.check_letter(lastname) == nil)
+			raise "Not valid name or surname or lastname [A-Z][a-z]+"
+		end
+		valid_extra_field(phone:phone,telegram:telegram,mail:mail,git:git)
+		self.surname = surname 
+		self.name = name 
 		self.lastname = lastname
 		if phone!=nil && Student.check_phone(phone) != nil then
 			self.phone = phone 
@@ -12,7 +16,12 @@ class Student
 			raise "Not valid phone! +[0-9]{11,13}"
 		end
 		self.telegram = telegram
-		self.mail = mail
+		if mail!=nil && Student.check_mail(phone) != nil then
+			self.mail = mail 
+		elsif(mail!=nil)
+			raise "Not valid mail! +[0-9]{11,13}"
+		end
+
 		self.git = git
 		self.ID = @@countStudents
 		@@countStudents = @@countStudents + 1
@@ -27,7 +36,44 @@ class Student
 		/\+?[0-9]{11,13}/.match(phone)
 	end
 
+	define_singleton_method :check_letter do |word|
+		/^[A-Z][a-z]+/.match(word)
+	end
+
+	define_singleton_method :check_mail do |mail|
+		/^[A-z0-9]+@[a-z0-9]+\.[a-z]+/.match(mail)
+	end
+
+	define_singleton_method :check_telegram do |telegram|
+		/^@[A-z0-9]/.match(telegram)
+	end
+	define_singleton_method :check_git do |git|
+		/^https:\/\/github\.com\/[A-z0-9]*\/[A-z0-9]*\.git/.match(git)
+	end
 	private
 	@@countStudents = 0
 	attr_writer :ID
+
+	def valid_extra_field(phone:nil,mail:nil,telegram:nil,git:nil)
+		if(phone!=nil)
+			if Student.check_phone(phone) ==nil then
+			 	raise "Not valid phone"
+			end
+		end
+		if(mail!=nil)
+			if(Student.check_mail(mail)==nil) then
+				raise "Not valid mail"
+			end
+		end
+		if(telegram!=nil)
+			if(Student.check_telegram(telegram)==nil) then
+				raise "Not valid telegram"
+			end
+		end
+		if(git!=nil)
+			if(Student.check_git(git)==nil) then
+				raise "Not valid git"
+			end
+		end
+	end
 end
