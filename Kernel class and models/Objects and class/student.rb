d1b25687868c6daade946453664f9b7c20bc7dcc
StrongerProgrammer7 <swat55551@gmail.com>
2023-02-22 19:09:42 +0300

    '#TDP- 2/3-4/4 Solution 3-4Task Avoid duplication constructor and get/set -> -> applied attr_accesor , attr_reader

--- Kernel class and models/Objects and class/student.rb
+++ Kernel class and models/Objects and class/student.rb
@@ -1,69 +1,22 @@
 class Student
-	#attr_accessor :surname, :name, :lastname, :phone, :telegram, :mail, :git
-	#attr_reader :ID
-	
+	attr_accessor :surname, :name, :lastname, :phone, :telegram, :mail, :git
+	attr_reader :ID
+
 	def initialize(surname,name,lastname, phone:nil, telegram:nil,mail:nil,git:nil)
-		@name = name
-		@surname = surname
-		@lastname = lastname
-		@phone = phone
-		@telegram = telegram
-		@mail = mail
-		@git = git
+		self.surname = surname
+		self.name = name
+		self.lastname = lastname
+		self.phone = phone
+		self.telegram = telegram
+		self.mail = mail
+		self.git = git
 		self.ID = @@countStudents
 		@@countStudents = @@countStudents + 1
 	end
 
-	def get_surname
-		@surname
-	end
-	def get_name
-		@name
-	end
-	def get_lastname
-		@lastname
-	end
-	def get_phone
-		@phone
-	end
-	def get_telegram
-		@telegram
-	end
-	def get_mail
-		@mail
-	end
-	def get_git
-		@git
-	end
-	def get_ID
-		@ID
-	end
-
-	def set_surname=(surname)
-		@surname=surname
-	end
-	def set_name=(name)
-		@name=name
-	end
-	def set_lastname=(lastname)
-		@lastname=lastname
-	end
-	def set_phone=(phone)
-		@phone=phone
-	end
-	def set_telegram=(telegram)
-		@telegram=telegram
-	end
-	def set_mail=(mail)
-		@mail=mail
-	end
-	def set_git=(git)
-		@git=git
-	end
-
 	def print_current_info()
 		print "ID\tname\tlastname\tphone\n"
-		print "#{get_ID}\t#{get_name}\t#{get_lastname}\t#{get_phone}\n"
+		print "#{self.ID}\t#{self.name}\t#{self.surname}\t#{self.phone}\n"
 	end
 
 	private
