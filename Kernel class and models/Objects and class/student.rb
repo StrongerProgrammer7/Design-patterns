8c16bf2abab6a3996be546162807c8e8c4d19696
StrongerProgrammer7 <swat55551@gmail.com>
2023-02-22 19:39:16 +0300

    '#TDP - 2/7/5 Using regular Using regular for check phone Using exception Raise for not valid phone

--- Kernel class and models/Objects and class/student.rb
+++ Kernel class and models/Objects and class/student.rb
@@ -6,7 +6,11 @@ class Student
 		self.surname = surname
 		self.name = name
 		self.lastname = lastname
-		self.phone = phone
+		if phone!=nil && Student.check_phone(phone) != nil then
+			self.phone = phone 
+		elsif(phone!=nil)
+			raise "Not valid phone! +[0-9]{11,13}"
+		end
 		self.telegram = telegram
 		self.mail = mail
 		self.git = git
@@ -17,6 +21,10 @@ class Student
 	def print_current_info()
 		print "ID\tname\tlastname\tphone\n"
 		print "#{self.ID}\t#{self.name}\t#{self.surname}\t#{self.phone}\n"
+	end
+
+	define_singleton_method :check_phone do |phone|
+		/\+?[0-9]{11,13}/.match(phone)
 	end
 
 	private
