9cab0728ab8413a57e783bf2f517eb5c10449092
StrongerProgrammer7 <swat55551@gmail.com>
2023-02-22 18:44:24 +0300

    '#TDP -2/1/2 Change Attr -> get set

--- Kernel class and models/Objects and class/student.rb
+++ Kernel class and models/Objects and class/student.rb
@@ -1,20 +1,70 @@
 class Student
-	attr_accessor :surname, :name, :lastname, :phone, :telegram, :mail, :git
-	attr_reader :ID
+	#attr_accessor :surname, :name, :lastname, :phone, :telegram, :mail, :git
+	#attr_reader :ID
 	
 	def initialize(surname,name,lastname, phone:nil, telegram:nil,mail:nil,git:nil)
-		self.name = name
-		self.surname = surname
-		self.lastname = lastname
-		self.phone = phone
-		self.telegram = telegram
-		self.mail = mail
-		self.git = git
+		@name = name
+		@surname = surname
+		@lastname = lastname
+		@phone = phone
+		@telegram = telegram
+		@mail = mail
+		@git = git
 		self.ID = @@countStudents
 		@@countStudents = @@countStudents + 1
+	end
+
+	def get_surname
+		@surname
+	end
+	def get_name
+		@name
+	end
+	def get_lastname
+		@lastname
+	end
+	def get_phone
+		@phone
+	end
+	def get_telegram
+		@telegram
+	end
+	def get_mail
+		@mail
+	end
+	def get_git
+		@git
+	end
+	def get_ID
+		@ID
+	end
+
+	def set_surname=(surname)
+		@surname=surname
+	end
+	def set_name=(name)
+		@name=name
+	end
+	def set_lastname=(lastname)
+		@lastname=lastname
+	end
+	def set_phone=(phone)
+		@phone=phone
+	end
+	def set_telegram=(telegram)
+		@telegram=telegram
+	end
+	def set_mail=(mail)
+		@mail=mail
+	end
+	def set_git=(git)
+		@git=git
 	end
 
 	private
 	@@countStudents = 0
 	attr_writer :ID
 end
+
+d = Student.new("v","v1","v2",phone:"24")
+p d.get_name,d.get_surname,d.get_phone
