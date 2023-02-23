926a69e9605fc453a1ddb164b9b876249566d8d8
StrongerProgrammer7 <swat55551@gmail.com>
2023-02-23 16:28:41 +0300

    '#TDP - 2/9/7 Add method validate add method get contacts and git return hash add method get_git add method get_any_contact 1.phone 2.telegram 3.mail

--- Kernel class and models/Objects and class/main.rb
+++ Kernel class and models/Objects and class/main.rb
@@ -1,9 +1,29 @@
 load './student.rb'
 
+def get_git(student)
+	student.git
+end
+def get_any_contacts(student)
+	if(student.phone != nil)
+		student.phone
+	elsif(student.telegram != nil)
+		student.telegram
+	elsif student.mail != nil
+		student.mail
+	end
+end
+			
 
-s1 = Student.new("Nn","Ssd","Lfg",phone:"87743258961")
-s2 = Student.new("Nv","Ssd","Lfg",phone:"+77743258961")
-s3 = Student.new("Ns","Ssd","Lfg",phone:"87743258961")
+def validate_contacts_git(student)
+	info = {}
+	info["git"] = get_git(student)
+	info["contact"] = get_any_contacts(student)
+	return info
+end
+
+s1 = Student.new("Nn","Ssd","Lfg",phone:"87743258961",git:"https://github.com/StPr/rep.git")
+s2 = Student.new("Nv","Ssd","Lfg",mail:"sw467@mail.ru")
+s3 = Student.new("Ns","Ssd","Lfg",telegram:"@sdsd")
 s4 = Student.new("Nd","Ssd","Lfg",phone:"+77743258961")
 s5 = Student.new("Nw","Ssd","Lfg",phone:"77743258961")
 s1.print_current_info
@@ -16,4 +36,6 @@ p Student.check_phone(s1.phone)
 s1.phone = "+77794563215"
 p Student.check_phone(s1.phone)
 
-p Student.check_mail("sdads2sda@mail.ru")
+p validate_contacts_git(s1)
+p validate_contacts_git(s2)
+p validate_contacts_git(s3)
