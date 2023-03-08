5587ba58faf213d59e41c9171fd806255090d394
StrongerProgrammer7 <swat55551@gmail.com>
2023-03-08 11:56:49 +0300

    '#TDP - 2/2/1-2 Add method Add constructor get string require string with , count>2 & <8 method valid => method.Class add method valid string => method.Class

---
+++ Kernel class and models/Reading<Viewing and Writing Entities/main.rb
@@ -0,0 +1,21 @@
+load './student.rb'
+
+
+s1 = Student.initialization("Nn,Ssd,Lfg,87743258961,https://github.com/StPr/rep.git")
+s2 = Student.initialization("Nv,Ssd,Lfg,swa@mail.ru")
+#s3 = Student.new(surname:"Ns",name:"Ssd",lastname:"Lfg",telegram:"@sdsd")
+#s4 = Student.new(surname:"Nd",name:"Ssd",lastname:"Lfg",phone:"+77743258961")
+#s5 = Student.new(surname:"Nw",name:"Ssd",lastname:"Lfg",phone:"77743258961",telegram:"@cad",mail:"s5@mail.com")
+print s1.to_s()
+print s2.to_s()
+#print s3.to_s()
+#print s4.to_s()
+#print s5.to_s()
+#p s1.name
+#p Student.check_phone(s1.phone)
+
+
+#s1.set_contacts(mail:"swa@mail.ru",telegram:"@cadet")
+#p s1.get_all_contacts()
+
+
---
+++ Kernel class and models/Reading<Viewing and Writing Entities/student.rb
@@ -0,0 +1,140 @@
+class Student
+	attr_accessor :surname, :name, :lastname
+	attr_reader :ID, :phone, :telegram, :mail, :git
+
+	def initialize(surname:,name:,lastname:, phone:nil, telegram:nil,mail:nil,git:nil)
+		#Student.valid_baseField_onCorrect(name:name,surname:surname,lastname:lastname)
+		#Student.valid_extraField_onCorrect(phone:phone,telegram:telegram,mail:mail,git:git)
+		self.surname = surname 
+		self.name = name 
+		self.lastname = lastname
+		self.phone = phone 
+		self.telegram = telegram
+		self.mail = mail 
+		self.git = git
+		self.ID = @@countStudents
+		@@countStudents = @@countStudents + 1
+	end
+
+	def Student.initialization(information)
+		raise "Not enough data or exists unnecessary data!(split [,])" if(information.count(",") < 2 || information.count(",") > 7)
+		hash_data = Student.valid_string_onCorrect(information.split(","))
+
+		Student.new(surname:hash_data["surname"],name:hash_data["name"],lastname:hash_data["lastname"],
+			phone:hash_data["phone"],mail:hash_data["mail"],telegram:hash_data["telegram"],git:hash_data["git"])
+	end
+	def to_s()
+		 #{}"ID\tname\tlastname\tphone\n"
+		 "#{self.ID}  #{self.name}  #{self.surname}  #{self.phone}  "
+	end
+
+	define_singleton_method :check_phone do |phone|
+		/\+?[0-9]{11,13}/.match(phone)
+	end
+
+	define_singleton_method :check_word do |word|
+		/^[A-Z][a-z]+/.match(word)
+	end
+
+	define_singleton_method :check_mail do |mail|
+		/^[A-z0-9]+@[a-z0-9]+\.[a-z]+/.match(mail)
+	end
+
+	define_singleton_method :check_telegram do |telegram|
+		/^@[A-z0-9]/.match(telegram)
+	end
+	define_singleton_method :check_git do |git|
+		/^https:\/\/github\.com\/[A-z0-9]*\/[A-z0-9]*\.git/.match(git)
+	end
+
+	def set_contacts(phone:nil,mail:nil,telegram:nil)
+		valid_extraField_onCorrect(phone:phone,mail:mail,telegram:telegram)		
+		if(phone!=nil) then
+			self.phone=phone 
+		end
+		if(mail!=nil) then
+			self.mail = mail
+		end
+		if(telegram!=nil) then
+			self.telegram = telegram
+		end
+	end
+
+	def get_all_contacts()
+		contacts = {}
+		contacts["phone"] =self.phone
+		contacts["telegram"] = self.telegram
+		contacts["mail"] = self.mail
+		return contacts
+	end
+
+	private
+	@@countStudents = 0
+	attr_writer :ID, :mail, :phone, :telegram, :git
+
+	def self.isNumeric(num)
+		num.split(//).each do |i|
+			return false if not(("0".."9").include?(i))
+		end
+		return true
+
+	end
+
+	def self.valid_string_onCorrect(data)
+		hash_data = Hash.new
+		Student.valid_baseField_onCorrect(name:data[0],surname:data[1],lastname:data[2])
+		hash_data["surname"] = data[1]
+		hash_data["name"]=data[0]
+		hash_data["lastname"] = data[2]
+		data.drop(3).each do |i|
+			next if(i==nil)
+			if(i.include? "@") then
+				(i[0]=='@') ? hash_data["telegram"] = i : hash_data["mail"] = i
+			end
+			hash_data["git"] = i if(i.include? "https:\/\/github") 
+			hash_data["phone"] = i if(isNumeric(i)==true )
+		end
+		Student.valid_extraField_onCorrect(phone:hash_data["phone"],mail:hash_data["mail"],telegram:hash_data["telegram"])
+		Student.valid_git(git:hash_data["git"])
+		return hash_data
+	end
+
+	def self.valid_contact(phone:nil,mail:nil,telegram:nil)
+		if(phone!=nil)
+			if Student.check_phone(phone) ==nil then
+			 	raise "Not valid phone"
+			end
+		end
+		if(mail!=nil)
+			if(Student.check_mail(mail)==nil) then
+				raise "Not valid mail"
+			end
+		end
+		if(telegram!=nil)
+			if(Student.check_telegram(telegram)==nil) then
+				raise "Not valid telegram"
+			end
+		end
+	end
+
+	def self.valid_git(git:nil)
+		if(git!=nil)
+			if(Student.check_git(git)==nil) then
+				raise "Not valid git"
+			end
+		end
+	end
+
+	def self.valid_extraField_onCorrect(phone:nil,mail:nil,telegram:nil,git:nil)
+		Student.valid_contact(phone:phone,mail:mail,telegram:telegram)
+		Student.valid_git(git:git)	
+	
+	end
+
+	def self.valid_baseField_onCorrect(name:,surname:,lastname:)
+		if( Student.check_word(surname) ==nil && Student.check_word(name)==nil && Student.check_word(lastname) == nil)
+			raise "Not valid name or surname or lastname [A-Z][a-z]+"
+		end
+	end
+
+end
