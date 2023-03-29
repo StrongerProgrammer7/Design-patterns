d78e79458f1c4510eb4792812a83450d6194cf97
StrongerProgrammer7 <swat55551@gmail.com>
2023-03-29 13:31:30 +0300

    '#TDP - 2/4/4 Refactoring Add super class <- get repeat code Delete reapeat code from inherit

--- Kernel class and models/Set clases/students_list_json.rb
+++ Kernel class and models/Set clases/students_list_json.rb
@@ -1,71 +1,16 @@
-load './data_list.rb'
 load './student.rb'
-load './student_short.rb'
+load './students_list_file.rb'
+
 require 'json'
 
 
-class Students_list_json
+class Students_list_json < Students_list_from_file
+
 	def initialize(addressFile)
 		list_students = read_from_json(addressFile)
 	end
 
-	def get_student_by_id(id)
-		list_students.each do |elem|
-			if(elem.id == id) then
-				return elem
-			end
-		end
-		return nil
-	end
-
-	def get_k_n_student_short_list(k,n,data_list:nil)
-		list_students_short = []
-		index_elem = 0
-		index_list = 0
-		list_students.each do |elem|
-			if(index_elem == n && index_list < k) then
-				index_list = index_list + 1
-				index_elem = 0
-			end
-			if(index_list == k && index_elem <= n) then
-				list_students_short.push(Student_short.initialization(elem))
-				if(index_elem == n)
-					break
-				end	
-			end
-			index_elem = index_elem + 1		
-		end
-
-
-		if(data_list == nil) then
-			return Data_list.new(list_students_short)
-		else
-			return data_list.list_entities = list_students_short
-		end
-	end
-
-	def sort_by_initials(data_list)
-		data_list.list_entities.sort! { |a,b| a.initials <=> b.initials}
-	end
-
-	def push_student(student)
-		id = list_students,max_by { |elem| elem.id }.id
-		student.id = id
-		list_students.push(student)
-	end
-
-	def replace_element_by_id(id,element)
-		list_students.map! { |elem| elem.id = id ? element : elem }
-	end
-
-	def delete_element_by_id(id)
-		list_students.delete_if { |elem| elem.id == id }
-	end
-
-	def get_student_short_count()
-		list_students.length
-	end
-
+	
 	def read_from_json(addressFile)
 		raise "Address file don't correct, check this." if(!File.exist?(addressFile))
 		
@@ -104,6 +49,4 @@ class Students_list_json
 		file.close
 	end
 
-	private
-		attr_accessor :list_students
 end
--- Kernel class and models/Set clases/students_list_txt.rb
+++ Kernel class and models/Set clases/students_list_txt.rb
@@ -1,9 +1,7 @@
-load './data_list.rb'
 load './student.rb'
-load './student_short.rb'
+load './students_list_file.rb'
 
-
-class Students_list_txt
+class Students_list_txt < Students_list_from_file
 	#plесь потом будет объект 
 	# Абстрактный класс который будет содержать методы reaad and write file
 	# Наследники от абстр реализ json yaml txt
@@ -14,62 +12,7 @@ class Students_list_txt
 	end
 
 	#реализовать сет объекта абстрак класса Student_file
-	def get_student_by_id(id)
-		list_students.each do |elem|
-			if(elem.id == id) then
-				return elem
-			end
-		end
-		return nil
-	end
-
-	def get_k_n_student_short_list(k,n,data_list:nil)
-		list_students_short = []
-		index_elem = 0
-		index_list = 0
-		list_students.each do |elem|
-			if(index_elem == n && index_list < k) then
-				index_list = index_list + 1
-				index_elem = 0
-			end
-			if(index_list == k && index_elem <= n) then
-				list_students_short.push(Student_short.initialization(elem))
-				if(index_elem == n)
-					break
-				end	
-			end
-			index_elem = index_elem + 1		
-		end
-
-
-		if(data_list == nil) then
-			return Data_list.new(list_students_short)
-		else
-			return data_list.list_entities = list_students_short
-		end
-	end
-
-	def sort_by_initials(data_list)
-		data_list.list_entities.sort! { |a,b| a.initials <=> b.initials}
-	end
-
-	def push_student(student)
-		id = list_students,max_by { |elem| elem.id }.id
-		student.id = id
-		list_students.push(student)
-	end
-
-	def replace_element_by_id(id,element)
-		list_students.map! { |elem| elem.id = id ? element : elem }
-	end
-
-	def delete_element_by_id(id)
-		list_students.delete_if { |elem| elem.id == id }
-	end
-
-	def get_student_short_count()
-		list_students.length
-	end
+	
 
 	def read_from_txt(addressFile)
 		raise "Address file don't correct, check this." if(!File.exist?(addressFile))
@@ -90,6 +33,4 @@ class Students_list_txt
 		file.close
 	end
 
-	private
-		attr_accessor :list_students
 end
