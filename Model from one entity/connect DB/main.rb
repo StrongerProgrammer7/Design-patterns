064d002bc63a984965135fe87d36a0f279620b84
StrongerProgrammer7 <swat55551@gmail.com>
2023-04-08 16:01:43 +0300

    '#TDP - 3/1/The end Add class Students_DB => method^ get_Instance - for singleton method crud for work with db static field mysql Class Students_list_DB all method call method Students_DB one filed dbconf link object Students_DB

--- Model from one entity/connect DB/main.rb
+++ Model from one entity/connect DB/main.rb
@@ -12,10 +12,14 @@ load './list_file/students_list_txt.rb'
 
 load './datatable/students_list.rb'
 
+load './student_list_DB.rb'
+
+
+=begin
 require 'mysql2'
 include Mysql2
 begin 
-	mysql = Mysql2::Client.new(:username => 'student', :password => '123', :host => 'localhost')
+	mysql = Mysql2::Client.new(:username => 'alex', :host => 'localhost')
 	mysql.query("USE Students")
 	mysql.query("CREATE TABLE IF NOT EXISTS \
 		Students(Id INT PRIMARY KEY AUTO_INCREMENT, Surname VARCHAR(30) NOT NULL, Name VARCHAR(30) NOT NULL,
@@ -26,22 +30,22 @@ begin
 	mysql.query("Alter table Students Change Lastname Lastname TEXT CHARACTER set utf8mb4 COLLATE utf8mb4_unicode_ci;")
 	text = File.read("./table_student/insert_students.sql")
 	mysql.query(text)
-	results = mysql.query("SELECT * FROM Students").to_a
+	#results = mysql.query("SELECT * FROM Students").to_a
 	
-	students = Array.new(results.length()) 
+	#students = Array.new(results.length()) 
 
-	results.each do |data|
-		students.push(Student.new(id:data['Id'],
-			surname: data['Surname'],
-			name: data['Name'],
-			lastname: data['Lastname'],
-			phone: data['phone'],
-			telegram: data['telegram'],
-			mail: data['mail'],
-			git: data['git']))
-	end
+	#results.each do |data|
+	#	students.push(Student.new(id:data['Id'],
+	#		surname: data['Surname'],
+	#		name: data['Name'],
+	#		lastname: data['Lastname'],
+	#		phone: data['phone'],
+	#		telegram: data['telegram'],
+	#		mail: data['mail'],
+	#		git: data['git']))
+	#end
 	
-	print students[0].surname,"\n"	,students.length()
+	#print students[0].surname,"\n"	,students.length()
 
 	#rs = mysql.query 'SELECT VERSION()'
 	#print rs.fetch_row
@@ -50,12 +54,24 @@ begin
 rescue Mysql2::Error => e
 	print e
 	#print e.Error
-
 ensure
 	
-	mysql.close
+	mysql&.close
 end
+=end
 
+=begin
+s1 = Student.initialization("8345,Volkov,John,Dmitrief,87743258961,,,https://github.com/StPr/rep.git")
+s2 = Student.initialization("8346,Lopy,John,Dmitrief,87743258961,,,https://github.com/StPr/rep.git")
+stdb = Students_list_DB.new()
+print s1.mail,"\n"
+stdb.push_student(s1)
+print stdb.get_student_short_count()
+stdb.delete_element_by_id(5)
+print stdb.get_student_short_count()
+stdb.replace_element_by_id(1,s2)
+print stdb.get_student_short_count()
+=end
 
 =begin
 print "initialization\n"
--- Model from one entity/connect DB/student_list_DB.rb
+++ Model from one entity/connect DB/student_list_DB.rb
@@ -1,45 +1,49 @@
-require_relative File.dirname($0) + '/data_list.rb'
-require_relative File.dirname($0) + './persons/student_short.rb'
-require_relative File.dirname($0) + './list_file/students_list_file.rb'
-require_relative File.dirname($0) + './list_file/students_list_txt.rb'
-require_relative File.dirname($0) + './list_file/students_list_json.rb'
-require_relative File.dirname($0) + './list_file/students_list_yaml.rb'
+require_relative File.dirname($0) + '/datatable/data_list.rb'
+require_relative File.dirname($0) + '/persons/student_short.rb'
 
+require 'mysql2'
+include Mysql2
 
-class Sutdents_list_DB
-	attr_reader :list_students
+class Students_DB
 
-	def initialize(list_students)
-		self.list_students = list_students
+	def self.getInstance()
+		@@mysql = Mysql2::Client.new(:username => 'alex', :host => 'localhost')
+		@@mysql.query("USE Students")
+		@inst = Students_DB.new() unless @inst	
+		return @inst
+	end
+			
+	def crud_student_by_db(query)
+		begin
+			@@mysql.query(query)
+		rescue Mysql2::Error => e
+			print e
+		end
 	end
 
+	private 
+		@@mysql = nil 
+		@inst = nil
+	
+end
+
+class Students_list_DB
+
+	def initialize()
+		@dbcon = Students_DB.getInstance()
+	end 
+	
 	def get_student_by_id(id)
-		self.list_students.each do |elem|
-			if(elem.id == id) then
-				return elem
-			end
-		end
-		return nil
+		return @dbcon.crud_student_by_db("Select * FROM Students WHERE id = #{id}").to_a
 	end
 
 	def get_k_n_student_short_list(k,n,data_list:nil)
+		offset = (k - 1) * n
+		limit = n
 		list_students_short = []
-		index_elem = 0
-		index_list = 0
-		self.list_students.each do |elem|
-			if(index_elem == n && index_list < k) then
-				index_list = index_list + 1
-				index_elem = 0
-			end
-			if(index_list == k && index_elem <= n) then
-				list_students_short.push(Student_short.initialization(elem))
-				if(index_elem == n)
-					break
-				end	
-			end
-			index_elem = index_elem + 1		
+		@dbcon.crud_student_by_db("Select * FROM Students LIMIT #{limit} OFFSET #{offset};").to_a.each do |elem|
+			list_students_short.push(Student_short.initialization(elem))
 		end
-
 
 		if(data_list == nil) then
 			return Data_list.new(list_students_short)
@@ -49,24 +53,28 @@ class Sutdents_list_DB
 	end
 
 	def push_student(student)
-		id = self.list_students,max_by { |elem| elem.id }.id
-		student.id = id
-		self.list_students.push(student)
+		@dbcon.crud_student_by_db("INSERT INTO Students(Surname, Name, Lastname, phone, mail, telegram,
+		git) VALUES ('#{student.surname}','#{student.name}','#{student.lastname}','#{student.phone || 'NULL'}','#{student.mail || 'NULL'}','#{student.telegram || 'NULL'}','#{student.git || 'NULL'}');")
 	end
 
 	def replace_element_by_id(id,element)
-		self.list_students.map! { |elem| elem.id = id ? element : elem }
+		@dbcon.crud_student_by_db("UPDATE Students 
+							SET Surname = '#{element.surname}', Name = '#{element.name}', 
+							Lastname = '#{element.lastname}',
+							phone = '#{element.phone}', mail = '#{element.mail}', 
+							git = '#{element.git}', telegram = '#{element.telegram}'
+							WHERE id = #{id};")
 	end
 
 	def delete_element_by_id(id)
-		self.list_students.delete_if { |elem| elem.id == id }
+		@dbcon.crud_student_by_db("Delete from Students WHERE id = #{id}")
 	end
 
 	def get_student_short_count()
-		self.list_students.length
+		return @dbcon.crud_student_by_db("Select count(*) FROM Students").to_a[0]['count(*)']
 	end
-
-	private
-	attr_writer :list_students
+	
+	private 
+		@dbcon = nil
 
 end
