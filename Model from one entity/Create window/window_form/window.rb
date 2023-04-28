d44720056d6d6978a98f58bbcef46210255e598e
StrongerProgrammer7 <swat55551@gmail.com>
2023-04-28 13:02:16 +0300

    '#TDP - 3/3/5.1 Add area filter Add field input name three radioButton add search field for git

--- Model from one entity/Create window/window_form/window.rb
+++ Model from one entity/Create window/window_form/window.rb
@@ -5,28 +5,57 @@ include Fox
 class MyMainWindow < FXMainWindow
   def initialize(app)
     # Call the base class initializer first
-    super(app, "My Application", :width => 800, :height => 800)
+    super(app, "My Application", :width => 1000, :height => 800)
 
     # Create a horizontal frame to hold the tab book and status bar
     horizontal_frame = FXHorizontalFrame.new(self, LAYOUT_SIDE_TOP|FRAME_NONE|LAYOUT_FILL_X|LAYOUT_FILL_Y)
 	
-	  
     # Create a tab book widget
     tab_book = FXTabBook.new(horizontal_frame, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y)
 
     # Create the first tab
     tab1 = FXTabItem.new(tab_book, "Tab 1")
 	
-	tab1_frame = FXVerticalFrame.new(tab_book, LAYOUT_FILL_X|LAYOUT_FILL_Y)
+	#FXVerticalFrame
+	tab1_frame = FXHorizontalFrame.new(tab_book, LAYOUT_FILL_X|LAYOUT_FILL_Y)
 	 # Create the filtering area on the left
     filtering_area = FXGroupBox.new(tab1_frame, "Filtering Area")
     FXLabel.new(filtering_area, "Filter by:")
-    FXTextField.new(filtering_area, 20)
+	FXLabel.new(filtering_area, "Last Name and Initials: ")
+    FXTextField.new(filtering_area, 15, :opts => TEXTFIELD_NORMAL)
 	
+	# Add a radio button group or a combo box widget to part 2 for selecting presence or absence of git
+	@filter_git = FXDataTarget.new(2)
+	git_group = FXGroupBox.new(filtering_area, "Наличие Гита", :opts => GROUPBOX_NORMAL|GROUPBOX_TITLE_LEFT|FRAME_GROOVE|LAYOUT_SIDE_TOP)
+    git_yes = FXRadioButton.new(git_group, "Есть",:target => @filter_git, :selector => FXDataTarget::ID_OPTION)
+    git_no = FXRadioButton.new(git_group, "Нет",:target => @filter_git, :selector => FXDataTarget::ID_OPTION+1)
+    git_dontcare = FXRadioButton.new(git_group, "Не важно",:target => @filter_git, :selector => FXDataTarget::ID_OPTION+2)
+ 	
+	# Add a text field widget to part 2 for entering text to search on the git
+    search_label = FXLabel.new(filtering_area, "Search: ")
+    search_field = FXTextField.new(filtering_area, 15, :opts => TEXTFIELD_NORMAL|TEXTFIELD_READONLY)
+    search_field.disable
+	
+	@filter_git.connect(SEL_COMMAND) do
+		puts "The newly selected value is #{@filter_git.value}"
+		search_field.enable if @filter_git.value == 0
+		search_field.disable if @filter_git.value >0 
+	end
+	
+	
+	№FXButton.new(filtering_area, "Apply")
+    №FXButton.new(filtering_area, "Reset")
 	# Create the table in the center
-    table = FXTable.new(tab1_frame, horizontal_frame, 0,
+    table = FXTable.new(tab1_frame, nil, 0,
       LAYOUT_SIDE_LEFT|LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_FILL_ROW)
-    table.setTableSize(10, 5)
+	table.visibleRows = 5
+    table.visibleColumns = 7
+    table.setTableSize(15, 8)
+	
+	#table.setColumnText(0, "Ruby 1.8.6")
+	#table.rowHeaderMode = LAYOUT_FIX_WIDTH
+	#table.rowHeaderWidth = 0
+	#table.columnHeaderMode = LAYOUT_FIX_HEIGHT
 	
 	# Create the control area on the right
     control_area = FXGroupBox.new(tab1_frame, "Control Area")
@@ -34,10 +63,6 @@ class MyMainWindow < FXMainWindow
     FXButton.new(control_area, "Edit")
     FXButton.new(control_area, "Delete")
 	
-    # Add a label to the first tab
-    label1 = FXLabel.new(tab1_frame, "This is tab 1")
-    label1.justify = JUSTIFY_CENTER_X|JUSTIFY_CENTER_Y
-
     # Create the second tab
     tab2 = FXTabItem.new(tab_book, "Tab 2")
 
@@ -62,7 +87,7 @@ class MyMainWindow < FXMainWindow
     # Add the tab book and close button to the main frame
     horizontal_frame.layoutHints |= LAYOUT_BOTTOM|LAYOUT_LEFT|LAYOUT_RIGHT|LAYOUT_FILL_X
     tab_book.layoutHints |= LAYOUT_FILL_X|LAYOUT_FILL_Y
-    close_button.layoutHints |= LAYOUT_BOTTOM|LAYOUT_RIGHT|LAYOUT_FILL_X
+    close_button.layoutHints |= LAYOUT_TOP|LAYOUT_RIGHT|LAYOUT_FILL_X
 	
     # Show the window
     show(PLACEMENT_SCREEN)
