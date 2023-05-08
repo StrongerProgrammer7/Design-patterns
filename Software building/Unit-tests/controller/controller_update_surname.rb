f52723705c52f34452d0854c21e0435886a2e8e9
StrongerProgrammer7 <swat55551@gmail.com>
2023-05-08 20:50:28 +0300

    '#TDP - 5/Unit-test delete not need require

--- Software building/Unit-tests/controller/controller_update_surname.rb
+++ Software building/Unit-tests/controller/controller_update_surname.rb
@@ -1,7 +1,4 @@
-require_relative File.dirname($0) + './view/student_list_view.rb'
-require_relative File.dirname($0) + './student_DB/student_list_DB.rb'
-require_relative File.dirname($0) + './student_list/student_list.rb'
-require_relative File.dirname($0) + './datatable/data_list_student_short.rb'
+require_relative File.dirname($0) + './persons/student.rb'
 require_relative File.dirname($0) + '/student_list_controller.rb'
 
 class Controller_update_surname< Student_list_controller
